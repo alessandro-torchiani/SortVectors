c338a70070211bd0c9a32d82deb57ec9986e7979
Alessandro Torchiani <ALESSANDRO.TORCHIANI@local.ittcesena.it>
2020-10-07 11:07:16 +0200

    First step

---
+++ SortVectors/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SortVectors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vet = new int[10] { 1, 2, 3, 23, 56, -34, -98, 87, 3, -10 };
+            for (int i = 0;i < 10; i++)
+            {
+                Console.WriteLine(vet[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
